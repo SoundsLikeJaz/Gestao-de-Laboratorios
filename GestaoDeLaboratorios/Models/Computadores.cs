db9b957c2b63791f1e6536640f6ab8cbe7af9aa8
Jackson Silva <jacksonslv36@gmail.com>
2024-03-18 13:25:04 -0300

    Adicionar arquivos de projeto.

---
+++ GestaoDeLaboratorios/Models/Computadores.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestaoDeLaboratorios.Models
+{
+    public class Computadores
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Processador { get; set; }
+        [Display(Name = "Placa Mãe")]
+        [Required]
+        public string PlacaMae { get; set; }
+        [Display(Name = "Memória")]
+        [Required]
+        public int Memoria { get; set; }
+        [Required]
+        public int HD { get; set; }
+        [Display(Name = "Número de Patrimônio")]
+        [Required]
+        public int NumeroPatrimonio { get; set; }
+        [Display(Name = "Data de Compra")]
+        [Required]
+        public DateOnly DataDeCompra { get; set; }
+
+    }
+}
